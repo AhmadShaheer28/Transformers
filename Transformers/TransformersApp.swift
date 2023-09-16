5e1647c6416c791b5fa43ada670ea69d1013e4f1
Ahmad Shaheer <ahmadshaheer28@gmail.com>
2023-09-16 19:19:30 +0500

    project setup

---
+++ Transformers/TransformersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransformersApp.swift
+//  Transformers
+//
+//  Created by Ahmad Shaheer on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TransformersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
